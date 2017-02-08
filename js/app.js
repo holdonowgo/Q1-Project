c01d5768aeae71b2100b28893e933ad9c4051b61
Randall Spencer <randallspencer@gmail.com>
2017-02-08 11:34:45 -0800

    Get skill images displayed below carousel.

--- js/app.js
+++ js/app.js
@@ -6,54 +6,56 @@ $(document).ready(function() {
     $('.scrollspy').scrollSpy();
     // $('.modal').modal();
     $('.modal').modal({
-      dismissible: true, // Modal can be dismissed by clicking outside of the modal
-      opacity: .5, // Opacity of modal background
-      inDuration: 300, // Transition in duration
-      outDuration: 200, // Transition out duration
-      startingTop: '4%', // Starting top style attribute
-      endingTop: '10%', // Ending top style attribute
-      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
-        // alert("Ready");
-        let id = trigger[0].id;
-        let itemHead = $('#modalItemName')[0];
-        let priHead = $('#modalPriHead')[0];
-        let priTxt = $('#modalPriTxt')[0];
-        let secHead = $('#modalSecHead')[0];
-        let secTxt = $('#modalSecTxt')[0];
-        let modalCardContent = $('#modalCardContent')[0];
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: .5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: '4%', // Starting top style attribute
+        endingTop: '10%', // Ending top style attribute
+        ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+            // alert("Ready");
+            let id = trigger[0].id;
+            let itemHead = $('#modalItemName')[0];
+            let priHead = $('#modalPriHead')[0];
+            let priTxt = $('#modalPriTxt')[0];
+            let secHead = $('#modalSecHead')[0];
+            let secTxt = $('#modalSecTxt')[0];
+            let modalCardContent = $('#modalCardContent')[0];
 
-        let itemParams = JSON.parse(localStorage.tooltipParams)[id];
-        console.log(itemParams);
-        // itemHead.innerText = itemParams.name;
-        // priTxt.innerText = '';
-        // secTxt.innerText = '';
-        $("#modalCardContent").empty();
-        $("#modalCardContent").append($(`<h5>${itemParams.name}</h5>`));
-        $("#modalCardContent").append($(`<h6>${itemParams.typeName}</h6>`));
-        $("#modalCardContent").append($(`<b>Primary</b><br/>`));
-        for(let primAttr of itemParams.attributes.primary) {
-          // priTxt.innerText += primAttr.text + ' ' + primAttr.color + '\n\r';
-          $("#modalCardContent").append($(`<i class="tiny material-icons">label</i><span style="color:${primAttr.color}">${primAttr.text}</span><br/>`));
-        }
+            let itemParams = JSON.parse(localStorage.tooltipParams)[id];
+            console.log(itemParams);
+            // itemHead.innerText = itemParams.name;
+            // priTxt.innerText = '';
+            // secTxt.innerText = '';
+            $("#modalCardContent").empty();
+            // $("#modalCardContent").attr('style',  `background-color:${itemParams.displayColor}`);
+            $("#modalCardContent").append($(`<h5>${itemParams.name}</h5>`));
+            $("#modalCardContent").append($(`<span style='color:${itemParams.displayColor}'><h6>${itemParams.typeName}</h6></span>`));
+            $("#modalCardContent").append($(`<b>Primary</b><br/>`));
+            for (let primAttr of itemParams.attributes.primary) {
+                // priTxt.innerText += primAttr.text + ' ' + primAttr.color + '\n\r';
+                $("#modalCardContent").append($(`<i class="tiny material-icons">label</i><span style="color:${primAttr.color}">${primAttr.text}</span><br/>`));
+            }
 
-        $("#modalCardContent").append($(`<br/><b>Secondary</b><br/>`));
-        for(let secAttr of itemParams.attributes.secondary) {
-          // secTxt.innerText += secAttr.text + ' ' + secAttr.color + '\n\r';
-          $("#modalCardContent").append($(`<i class="tiny material-icons">label_outline</i><span style="color:${secAttr.color}">${secAttr.text}</span><br/>`));
-        }
+            $("#modalCardContent").append($(`<br/><b>Secondary</b><br/>`));
+            for (let secAttr of itemParams.attributes.secondary) {
+                // secTxt.innerText += secAttr.text + ' ' + secAttr.color + '\n\r';
+                $("#modalCardContent").append($(`<i class="tiny material-icons">label_outline</i><span style="color:${secAttr.color}">${secAttr.text}</span><br/>`));
+            }
 
-        $("#modalCardContent").append($(`<br/><b>Passive</b><br/>`));
-        for(let pasAttr of itemParams.attributes.passive) {
-          // secTxt.innerText += secAttr.text + ' ' + secAttr.color + '\n\r';
-          $("#modalCardContent").append($(`<p style="color:${pasAttr.color}">${pasAttr.text}</p>`));
-        }
+            if (itemParams.attributes.passive.length) {
+                $("#modalCardContent").append($(`<br/><b>Passive</b><br/>`));
+                for (let pasAttr of itemParams.attributes.passive) {
+                    // secTxt.innerText += secAttr.text + ' ' + secAttr.color + '\n\r';
+                    $("#modalCardContent").append($(`<p style="color:${pasAttr.color}">${pasAttr.text}</p>`));
+                }
+            }
 
-        $("#modalCardContent").append($(`<br/><i>${itemParams.flavorText}</i>`));
-        $('#card-image').attr('src', `http://media.blizzard.com/d3/icons/items/large/${itemParams.icon}.png?locale=en_US&apikey=${localStorage.apiKey}`)
-      },
-      // complete: function() { alert('Closed'); } // Callback for Modal close
-    }
-  );
+            $("#modalCardContent").append($(`<br/><i>${itemParams.flavorText}</i>`));
+            $('#card-image').attr('src', `http://media.blizzard.com/d3/icons/items/large/${itemParams.icon}.png?locale=en_US&apikey=${localStorage.apiKey}`)
+        },
+        // complete: function() { alert('Closed'); } // Callback for Modal close
+    });
 });
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
@@ -71,35 +73,35 @@ let key = localStorage.apiKey;
 let statLookup = {
     "life": 'LIFE',
     "damage": 'DAMAGE',
-    "toughness": 'TOUGH',
-    "healing": 'HEAL',
-    "attackSpeed": 'ATK SPD',
+    "toughness": 'TOUGHNESS',
+    "healing": 'HEALING',
+    "attackSpeed": 'ATTACK SPEED',
     "armor": 'ARMOR',
     "strength": 'STRENGTH',
     "dexterity": 'DEXTERITY',
-    "vitality": 'VIT',
-    "intelligence": 'INT',
-    "physicalResist": 'PHYS RES',
-    "fireResist": 'FIRE RES',
-    "coldResist": 'COLD RES',
-    "lightningResist": 'LIT RES',
-    "poisonResist": 'POIS RES',
-    "arcaneResist": 'ARC RES',
-    "critDamage": 'CRIT DMG',
-    "blockChance": 'BLK %',
-    "blockAmountMin": 'BLK MIN',
-    "blockAmountMax": 'BLK MAX',
-    "damageIncrease": 'DMG INC',
-    "critChance": 'CRIT %',
-    "damageReduction": 'DMG RED',
-    "thorns": 'THORN',
-    "lifeSteal": 'LSTEAL',
-    "lifePerKill": 'LPK',
-    "goldFind": 'GOLD FND',
-    "magicFind": 'MGK FND',
-    "lifeOnHit": 'LOH',
-    "primaryResource": 'PRI RESOURCE',
-    "secondaryResource": 'SEC RESOURCE'
+    "vitality": 'VITALITY',
+    "intelligence": 'INTELLIGENCE',
+    "physicalResist": 'PHYSICAL RESISTANCE',
+    "fireResist": 'FIRE RESISTANCE',
+    "coldResist": 'COLD RESISTANCE',
+    "lightningResist": 'LIGHTNING RESISTANCE',
+    "poisonResist": 'POISON RESISTANCE',
+    "arcaneResist": 'ARCANE RESISTANCE',
+    "critDamage": 'CRITICAL DAMAGE',
+    "blockChance": 'BLOCK %',
+    "blockAmountMin": 'BLOCK MIN',
+    "blockAmountMax": 'BLOCK MAX',
+    "damageIncrease": 'DAMAGE INCREASE',
+    "critChance": 'CRITICAL HIT %',
+    "damageReduction": 'DAMAGE REDUCTION',
+    "thorns": 'THORNS',
+    "lifeSteal": 'LIFE STEAL',
+    "lifePerKill": 'LIFE PER KILL',
+    "goldFind": 'GOLD FIND',
+    "magicFind": 'MGK FIND',
+    "lifeOnHit": 'LIFE ON HIT',
+    "primaryResource": 'PRIMARY RESOURCE',
+    "secondaryResource": 'SECONDARY RESOURCE'
 };
 
 loadGear(name, tag, key);
@@ -221,48 +223,35 @@ function bindHeroData(heroId) {
                 });
             }
             localStorage['heroItems'] = JSON.stringify(heroJson.items);
-            return heroJson.items;
+            localStorage['heroSkills'] = JSON.stringify(heroJson.skills);
+            return heroJson;
         })
-        .then(function(itemsJson) {
-            let itemDetailsObj = {};
-            for (let key in itemsJson) {
+        .then(function(heroJson) {
+            for (let key in heroJson.items) {
                 let flavorText;
-                let url = `https://us.api.battle.net/d3/data/${itemsJson[key].tooltipParams}?locale=en_US&apikey=${localStorage.apiKey}`;
+                let url = `https://us.api.battle.net/d3/data/${heroJson.items[key].tooltipParams}?locale=en_US&apikey=${localStorage.apiKey}`;
                 fetch(url)
                     .then(function(response) {
                         return response.json();
                     })
                     .then(function(itemJson) {
-                          let tooltipParams = JSON.parse(localStorage.tooltipParams);
-                          tooltipParams[itemJson.id] = itemJson;
-                          localStorage.tooltipParams = JSON.stringify(tooltipParams);
-                      // console.log(itemJson);
-                    //     let tooltipParamsObj = {};
-                    //
-                    //     tooltipParamsObj['id'] = itemJson.id;
-                    //     tooltipParamsObj['name'] = itemJson.name;
-                    //     tooltipParamsObj['flavorText'] = itemJson.flavorText;
-                    //     tooltipParamsObj['displayColor'] = itemJson.displayColor;
-                    //     tooltipParamsObj['typeName'] = itemJson.typeName;
-                    //
-                    //     return tooltipParamsObj;
-                    // })
-                    // .then(function(tooltipParamsObj) {
+                        let tooltipParams = JSON.parse(localStorage.tooltipParams);
+                        tooltipParams[itemJson.id] = itemJson;
+                        localStorage.tooltipParams = JSON.stringify(tooltipParams);
                         $(`#${key}`).empty();
 
                         let width = key === 'leftFinger' || key === 'rightFinger' || key === 'waist' || key === 'neck' ? '64px' : '64px';
                         let height = key === 'leftFinger' || key === 'rightFinger' || key === 'waist' || key === 'neck' ? '64px' : '128px';
-                        let src = `http://media.blizzard.com/d3/icons/items/large/${itemsJson[key].icon}.png?locale=en_US&apikey=${localStorage.apiKey}`;
-                        // console.log(itemsJson);
+                        let src = `http://media.blizzard.com/d3/icons/items/large/${heroJson.items[key].icon}.png?locale=en_US&apikey=${localStorage.apiKey}`;
                         $(`#${key}`)
                             .append($('<div class="card-image waves-effect waves-block waves-light">')
-                                .append($(`<img class="activator" style="width:${width};height:${height};margin:0px auto;display:block;border:solid;border-color:${itemsJson[key].displayColor};vertical-align:text-bottom" src=${src}>`))
+                                .append($(`<img class="activator" style="width:${width};height:${height};margin:0px auto;display:block;border:solid;border-color:${heroJson.items[key].displayColor};vertical-align:text-bottom" src=${src}>`))
                             )
                             .append($('<div class="card-content center">')
                                 .append($('<span class="card-title activator grey-text text-darken-4" style="text-align:center; font-size:medium">')
                                     .text(itemJson.name)
                                 )
-                                .append($(`<a class="waves-effect waves-light btn" href="#modalStats" id="${itemJson.id}">Stats</a>`))
+                                .append($(`<a class="waves-effect waves-light btn blue-grey" href="#modalStats" id="${itemJson.id}">Stats</a>`))
                             )
                             .append($('<div class="card-reveal">')
                                 .append($(`<span class="card-title grey-text text-darken-4" style="text-align:center; font-size:medium;">`)
@@ -272,14 +261,45 @@ function bindHeroData(heroId) {
                                     .text(itemJson.flavorText)
                                 )
                             );
+                    })
+                // .then(function(tooltipParamsObj) {
+                //     let tooltipParams = JSON.parse(localStorage.tooltipParams);
+                //     tooltipParams[tooltipParamsObj.id] = tooltipParamsObj;
+                //     localStorage.tooltipParams = JSON.stringify(tooltipParams);
+                // });
+            }
 
-                        // return tooltipParamsObj;
-                    });
-                    // .then(function(tooltipParamsObj) {
-                    //     let tooltipParams = JSON.parse(localStorage.tooltipParams);
-                    //     tooltipParams[tooltipParamsObj.id] = tooltipParamsObj;
-                    //     localStorage.tooltipParams = JSON.stringify(tooltipParams);
-                    // });
+            return heroJson;
+        })
+        .then(function(heroJson) {
+            console.log(heroJson.skills.active);
+            for (let skillObj of heroJson.skills.active) {
+              console.log(skillObj);
+                let iconSkill = skillObj.skill.icon;
+                let srcSkill = `http://media.blizzard.com/d3/icons/skills/64/${iconSkill}.png?locale=en_US&apikey=${localStorage.apiKey}`;
+                console.log(srcSkill);
+
+                $('#tblRowSkills').append(`<td>`).append(`<img src=${srcSkill}>`);
+
+
+                // $(`#${key}`)
+                //     .append($('<div class="card-image waves-effect waves-block waves-light">')
+                //         .append($(`<img class="activator" style="width:${width};height:${height};margin:0px auto;display:block;border:solid;border-color:${heroJson.items[key].displayColor};vertical-align:text-bottom" src=${src}>`))
+                //     )
+                //     .append($('<div class="card-content center">')
+                //         .append($('<span class="card-title activator grey-text text-darken-4" style="text-align:center; font-size:medium">')
+                //             .text(itemJson.name)
+                //         )
+                //         .append($(`<a class="waves-effect waves-light btn blue-grey" href="#modalStats" id="${itemJson.id}">Stats</a>`))
+                //     )
+                //     .append($('<div class="card-reveal">')
+                //         .append($(`<span class="card-title grey-text text-darken-4" style="text-align:center; font-size:medium;">`)
+                //             .text(itemJson.typeName)
+                //         )
+                //         .append($('<p>')
+                //             .text(itemJson.flavorText)
+                //         )
+                //     );
             }
         });
 }
